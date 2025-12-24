1173d4b9660fa4102da6407f9b318d7a01a1f233
chaeyn <chaulfe@gmail.com>
2025-12-25 01:06:02 +0900

    삭제 기능

--- src/main/java/com/study/shop/item/ItemController.java
+++ src/main/java/com/study/shop/item/ItemController.java
@@ -3,11 +3,15 @@ package com.study.shop.item;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -62,5 +66,11 @@ public class ItemController {
   String editItem(Long id, String title, Integer price) {
     itemService.editItem(id, title, price);
     return "redirect:/list";
+  }
+
+  @DeleteMapping("item")
+  ResponseEntity<String> deleteItem(@RequestParam Long id){
+      itemRepository.deleteById(id);
+      return ResponseEntity.status(200).body("삭제완료");
   }
 }
--- src/main/java/com/study/shop/post/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.study.shop.post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
