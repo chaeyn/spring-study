1494c1edbe58af970fb9c188c6a4c4fb0d2fff6c
chaeyn <chaulfe@gmail.com>
2025-12-26 00:02:34 +0900

    DTO

--- src/main/java/com/study/shop/member/MemberController.java
+++ src/main/java/com/study/shop/member/MemberController.java
@@ -1,16 +1,21 @@
 package com.study.shop.member;
 
+import java.util.HashMap;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
   private final MemberService memberService;
+  private final MemberRepository memberRepository;
 
   @GetMapping("register")
   String register(Authentication auth) {
@@ -38,5 +43,26 @@ public class MemberController {
       return "my-page.html";
     }
     return "redirect:/list";
+  }
+
+  @GetMapping("/user/{id}")
+  @ResponseBody
+  public MemberDto getUser(@PathVariable Long id) {
+    Optional<Member> result = memberRepository.findById(id);
+
+      Member member = result.get();
+      MemberDto data = new MemberDto(member.getUsername(), member.getDisplayName());
+
+      return data;
+  }
+}
+
+class MemberDto {
+  public String username;
+  public String displayName;
+
+  MemberDto(String username, String displayName) {
+    this.username = username;
+    this.displayName = displayName;
   }
 }
